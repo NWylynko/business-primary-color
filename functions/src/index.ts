23fb8a5ef3cff12fec8c7df9b3c08a3ca8591a36
Nick Wylynko <nick1014375@gmail.com>
2021-03-23 09:12:50 +0800

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,21 +2,13 @@ import * as functions from "firebase-functions";
 import { promises as fs } from 'fs';
 import * as path from "path"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 interface Data {
   [x: string]: string;
 }
 
-export default functions.https.onRequest(async (request, response) => {
+const data: Data = JSON.parse(await fs.readFile(path.resolve('./data.json'), 'utf-8'));
 
-  const data: Data = JSON.parse(await fs.readFile(path.resolve('./data.json'), 'utf-8'));
+export default functions.https.onRequest(async (request, response) => {
 
   const name = request.query.name?.toString();
 
